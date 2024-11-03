c025a423d60d7d6295b59b883d81707e26ebe5cd
Thomas Lenz <Thomas.Lenz85@gmail.com>
2024-11-03 12:09:26 +0100

    Add model classes to db context, add db migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Bill_o_Pro.Models;
 
 namespace Bill_o_Pro.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceItem> InvoiceItems { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Mandatee> Mandatees { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<User> Users { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- Data/Migrations/20241103110614_addedModelTables.Designer.cs
+++ Data/Migrations/20241103110614_addedModelTables.Designer.cs
@@ -4,6 +4,7 @@ using Bill_o_Pro.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Bill_o_Pro.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241103110614_addedModelTables")]
+    partial class addedModelTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
