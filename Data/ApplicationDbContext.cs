0eda486224085cb8bdff5029ecc452a05e09619b
Thomas Lenz <Thomas.Lenz85@gmail.com>
2024-11-06 17:41:18 +0100

    Update DbContext with new class names

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,16 +6,16 @@ namespace Bill_o_Pro.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public DbSet<Address> Addresses { get; set; }
-        public DbSet<BankAccount> BankAccounts { get; set; }
-        public DbSet<Client> Clients { get; set; }
-        public DbSet<Invoice> Invoices { get; set; }
-        public DbSet<InvoiceItem> InvoiceItems { get; set; }
-        public DbSet<Item> Items { get; set; }
-        public DbSet<Mandatee> Mandatees { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderItem> OrderItems { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<BankAccount> BankAccount { get; set; }
+        public DbSet<Client> Client { get; set; }
+        public DbSet<Invoice> Invoice { get; set; }
+        public DbSet<InvoiceItem> InvoiceItem { get; set; }
+        public DbSet<Item> Item { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<User> User { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
