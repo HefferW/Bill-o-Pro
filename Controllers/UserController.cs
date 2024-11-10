01740d9dbabe34cde55daabbfe5b319b35c50f61
Thomas Lenz <Thomas.Lenz85@gmail.com>
2024-11-10 20:36:30 +0100

    modify view

---
+++ Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Bill_o_Pro.Data;
+using Bill_o_Pro.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bill_o_Pro.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var userListFromDb = _context.Users.ToList();
+            return View(userListFromDb);
+        }
+
+        public IActionResult ShowUserDetails(string id)
+        {
+            return View();
+        }
+        public IActionResult CreateEditUser(User user)
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
