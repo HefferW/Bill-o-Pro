9b52ab0477e54e54d814d62e56c71ea0e889f5f1
Thomas Lenz <Thomas.Lenz85@gmail.com>
2024-11-17 14:52:17 +0100

    modify db-migrations, add view for clients

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,66 @@
+﻿using Bill_o_Pro.Data;
+using Bill_o_Pro.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bill_o_Pro.Controllers
+{
+    public class ClientController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClientController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var clientListFromDb = _context.Client.ToList();
+            return View(clientListFromDb);
+        }
+        public IActionResult ShowClientDetails(string id)
+        {
+            if (id == null)
+            {
+                return View();
+            }
+            Guid clientId = new Guid(id);
+            var dbClient = _context.Client.SingleOrDefault(x => x.Id == clientId);
+            return View(dbClient);
+        }
+        public IActionResult CreateEditClient(Client client)
+        {
+            if (client == null)
+            {
+                return RedirectToAction("Index");
+            }
+            // Create new client
+            if (client.Id.ToString() == "00000000-0000-0000-0000-000000000000") //{00000000-0000-0000-0000-000000000000}
+            {
+                // TODO: Check for valid client number
+                client.Id = Guid.NewGuid();
+                _context.Client.Add(client);
+            }
+            // Update existing client
+            else
+            {
+                var dbClient = _context.Client.SingleOrDefault(x => x.Id == client.Id);
+                if (dbClient == null)
+                {
+                    Console.WriteLine("An error occured. Can not find entry in database");
+                    return NotFound();
+                }
+
+                // TODO: Add missing properties
+                dbClient.Number = client.Number;
+                dbClient.CompanyName = client.CompanyName;
+                dbClient.Name = client.Name;
+                dbClient.Phone = client.Phone;
+                dbClient.Mobile = client.Mobile;
+                dbClient.Email = client.Email;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -3,16 +3,16 @@
     public class Invoice
     {
         public Guid Id { get; set; }
-        public Client Client { get; set; }
-        public User User { get; set; }
+        public Client? Client { get; set; }
+        public User? User { get; set; }
         public DateOnly Date { get; set; }
         public DateOnly DeliveryDate { get; set; }
         public DateOnly payDate { get; set; }
-        public List<InvoiceItem> Bom { get; set; }
+        public List<InvoiceItem>? Bom { get; set; }
         public string Number { get; set; }
         public int Tax { get; set; }
         public string Email { get; set; }
         public float Total { get; set; }
-        public List<Order> Order { get; set; }
+        public List<Order>? Order { get; set; }
     }
 }
